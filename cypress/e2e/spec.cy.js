272dcfbb7b2d508bed9a38c4f7d91ff43e7182cd
Fionna Chan <fionnacst@gmail.com>
2022-10-28 11:58:51 +0100

    create minimal reproducible example

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,9 @@
+describe("Home Page", () => {
+  it(".wrapper should have a scrollTop of 0", () => {
+    cy.visit("/");
+    cy.get("input").type("something");
+    cy.get("main").invoke("scrollTop").should("eq", 0);
+    /* cypress would move main's scrollTop to between the partially-hidden-element's bottom absolute value and the main's padding value (whichever smaller) */
+    cy.pause();
+  });
+});
